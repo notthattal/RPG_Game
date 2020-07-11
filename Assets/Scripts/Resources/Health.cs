ddf4b7876b997911707b73a14662309e9337520c
Tal Erez <>
2020-07-11 13:18:07 -0400

    Implemented Damage Text

--- Assets/Scripts/Resources/Health.cs
+++ Assets/Scripts/Resources/Health.cs
@@ -4,12 +4,18 @@ using RPG.Saving;
 using RPG.Core;
 using RPG.Stats;
 using GameDevTV.Utils;
+using UnityEngine.Events;
 
 namespace RPG.Resources
 {
     public class Health : MonoBehaviour, ISaveable
     {
         [SerializeField] float regnerationPercentage = 70f;
+        [SerializeField] TakeDamageEvent takeDamage = default;
+
+        [System.Serializable]
+        public class TakeDamageEvent : UnityEvent<float>
+        {}
 
         LazyValue<float> healthPoints;
 
@@ -50,10 +56,15 @@ namespace RPG.Resources
             print(gameObject.name + "took damage: " + damage);
 
             healthPoints.value = Mathf.Max(healthPoints.value - damage, 0);
+
             if (healthPoints.value == 0)
             {
                 Die();
                 AwardExperience(instigator);
+            }
+            else
+            {
+                takeDamage.Invoke(damage);
             }
         }
 
